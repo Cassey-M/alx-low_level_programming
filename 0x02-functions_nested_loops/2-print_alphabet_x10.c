60a630be4c0d98f1bff8d2d010a557643a0fab4f
Cassey-M <murongacassey@gmail.com>
2023-04-26 10:54:23 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,17 @@
 */
 void print_alphabet_x10(void)
 {
-    int i = 0;
-    while (i < 10)
-    {
-        char letter = 'a';
-        while (letter <= 'z')
-        {
-            putchar(letter);
-            letter++;
-        }
-        putchar('\n');
-        i++;
+int i = 0;
+while (i < 10)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
 }
-_putchar('\n');
+putchar('\n');
+i++;
+}
+putchar('\n');
 }
