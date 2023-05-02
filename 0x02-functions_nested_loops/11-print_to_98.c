5a574f7b8df26776b23ff755596e70cd236e4b9c
Cassey-M <murongacassey@gmail.com>
2023-05-01 18:07:18 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,44 +1,42 @@
 #include <stdio.h>
 /**
 * print_to_98 - print all numbers from input to 98
-*@n: the starting number
+* @n: the starting number
 */
 int _putchar(char c);
 void print_number(int n);
-
 void print_to_98(int n)
 {
-    int i;
-    if (n <= 98)
-    {
-        for (i = n; i < 98; i++)
-        {
-            print_number(i);
-            _putchar(',');
-            _putchar(' ');
-        }
-    }
-    else
-    {
-        for (i = n; i > 98; i--)
-        {
-            print_number(i);
-            _putchar(',');
-            _putchar(' ');
-        }
-    }
-    print_number(98);
-    _putchar('\n');
+int i;
+if (n <= 98)
+{
+for (i = n; i < 98; i++)
+{
+print_number(i);
+_putchar(',');
+_putchar(' ');
 }
-
+}
+else
+{
+for (i = n; i > 98; i--)
+{
+print_number(i);
+_putchar(',');
+_putchar(' ');
+}
+}
+print_number(98);
+_putchar('\n');
+}
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10)
-        print_number(n / 10);
-    _putchar(n % 10 + '0');
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10)
+print_number(n / 10);
+_putchar(n % 10 + '0');
 }
