aacd619d478726413fb7af352f639fdad7baaa49
Cassey-M <murongacassey@gmail.com>
2023-06-01 05:44:08 -0700

    linked_lists

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -7,7 +7,7 @@
  * Return: On success, return the number of characters written.
  *         On error, return -1 and errno is set appropriately.
  */
-int _putchar(char c)
+int _putchar(char *c)
 {
 	return (write(1, &c, 1));
 }
