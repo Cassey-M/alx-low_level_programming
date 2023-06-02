ad2e0b9e70ca3f59b567ca344e9fb64aca605ead
Cassey-M <murongacassey@gmail.com>
2023-06-02 07:27:19 -0700

    argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,23 +1,41 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 /**
-* main - Prints the name of the program.
-* @argc: The number of command-line arguments.
-* @argv: The array of command-line argument strings.
-* Return: 0 indicating successful execution.
+* main - Entry point of the program
+* @argc: The number of command-line arguments
+* @argv: An array of strings containing the command-line arguments
+*
+* Return: Always 0 (indicating successful execution)
 */
 int main(int argc, char *argv[])
 {
-char *program_name = strdup(argv[0]);
-char *file_name = strrchr(program_name, '/');
-if (file_name != NULL)
-{
-file_name[0] ='\0';
-file_name++;
-}
-printf("%s\n", program_name);
-free(program_name);
+char *programName;
 (void)argc;
+programName = strrchr(argv[0], '/');
+if (programName == NULL) 
+{
+programName = argv[0];
+}
+else
+{
+programName++;
+}
+while (*programName)
+{
+_putchar(*programName);
+programName++;
+}
+_putchar('\n');
 return (0);
+}
+/**
+* _putchar - Prints a character to the standard output
+* @c: The character to be printed
+*
+* Return: The printed character as an unsigned char cast to an int
+*/
+int _putchar(char c)
+{
+return putchar(c);
 }
