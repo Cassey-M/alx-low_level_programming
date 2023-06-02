e1b0997d174f109ffd6e2dfea27e79b5f7dc30d8
Cassey-M <murongacassey@gmail.com>
2023-06-01 18:23:04 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,56 @@
-#include <stdio.h>
+#include <stddef.h>
 #include "lists.h"
+void print_number(size_t n);
 /**
-* print_list -  Prints all the elements of a linked list.
+* print_list - Prints all the elements of a list_t list.
+* @h: Pointer to the head of the list.
 *
-* @h: A pointer to the head of the linked list.
-* Return: The number of nodes in the linked list.
-*
-* Prints the elements of the linked list in the following format:
-*   [0] <str_value_of_node_0>
-*   [1] <str_value_of_node_1>
-*   ...
-*
-* If the string value of a node is NULL, it prints "(nil)" instead.
-* Returns the total number of nodes in the linked list.
+* Return: The number of nodes.
 */
 size_t print_list(const list_t *h)
 {
-const list_t *current = h; /* Start from the head of the list */
-size_t count = 0; /* Initialize count to 0 */
-while (current != NULL)
+size_t count = 0;
+size_t i;
+while (h != NULL)
 {
-if (current->str != NULL)
+if (h->str == NULL)
 {
-printf("[%ld] %s\n", count, current->str);
+_putchar('[');
+_putchar('0');
+_putchar(']');
+_putchar(' ');
+_putchar('(');
+_putchar('n');
+_putchar('i');
+_putchar('l');
+_putchar(')');
+_putchar('\n');
 }
 else
 {
-printf("[%ld] (nil)\n", count);
+size_t len = 0;
+while (h->str[len] != '\0')
+len++;
+_putchar('[');
+print_number(len);
+_putchar(']');
+_putchar(' ');
+for (i = 0; h->str[i] != '\0'; i++)
+_putchar(h->str[i]);
+_putchar('\n');
 }
-count++; /* Increment the count for each node */
-current = current->next; /*  Move to the next node */
+count++;
+h = h->next;
 }
 return (count);
+}
+/**
+* print_number - Prints a positive integer using _putchar.
+* @n: The number to be printed.
+*/
+void print_number(size_t n)
+{
+if (n / 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
 }
