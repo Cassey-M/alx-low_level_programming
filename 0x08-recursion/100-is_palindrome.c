e07b70e464b8631694e6b96c02954a6ab9876ec7
Cassey-M <murongacassey@gmail.com>
2023-06-02 01:18:13 -0700

    sion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,42 @@
 #include "main.h"
-
 int _strlen_recursion(char *s);
 int check_palindrome(char *s, int len);
-
 /**
- * is_palindrome - Checks if a string is a palindrome.
- * @s: The string to be checked.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
+* is_palindrome - Checks if a string is a palindrome.
+* @s: The string to be checked.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
 int is_palindrome(char *s)
 {
-    int len = 0;
-
-    len = _strlen_recursion(s);
-    return (check_palindrome(s, len));
+int len = 0;
+len = _strlen_recursion(s);
+return (check_palindrome(s, len));
 }
-
 /**
- * check_palindrome - Recursive helper function to check if a string is a palindrome.
- * @s: The string to be checked.
- * @len: The length of the string.
- *
- * Return: 1 if the string is a palindrome, 0 otherwise.
- */
+* check_palindrome - Recursive helper function to check if a string is a palindrome.
+* @s: The string to be checked.
+* @len: The length of the string.
+*
+* Return: 1 if the string is a palindrome, 0 otherwise.
+*/
 int check_palindrome(char *s, int len)
 {
-    if (len <= 1)
-        return (1);
-    if (*s == *(s + len - 1))
-        return (check_palindrome(s + 1, len - 2));
-    return (0);
+if (len <= 1)
+return (1);
+if (*s == *(s + len - 1))
+return (check_palindrome(s + 1, len - 2));
+return (0);
 }
-
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to measure.
- *
- * Return: The length of the string.
- */
+* _strlen_recursion - Returns the length of a string.
+* @s: The string to measure.
+*
+* Return: The length of the string.
+*/
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,29 @@
 #include "main.h"
-
 int check_prime(int n, int divisor);
-
 /**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to be checked.
- *
- * Return: 1 if the number is prime, 0 otherwise.
- */
+* is_prime_number - Checks if a number is prime.
+* @n: The number to be checked.
+*
+* Return: 1 if the number is prime, 0 otherwise.
+*/
 int is_prime_number(int n)
 {
-    return (check_prime(n, 2));
+return (check_prime(n, 2));
 }
-
 /**
- * check_prime - Recursive helper function to check if a number is prime.
- * @n: The number to be checked.
- * @divisor: The current divisor to check.
- *
- * Return: 1 if the number is prime, 0 otherwise.
- */
+* check_prime - Recursive helper function to check if a number is prime.
+* @n: The number to be checked.
+* @divisor: The current divisor to check.
+*
+* Return: 1 if the number is prime, 0 otherwise.
+*/
 int check_prime(int n, int divisor)
 {
-    if (n <= 1)
-        return (0);
-    if (n % divisor == 0 && divisor != n)
-        return (0);
-    if (divisor > n / 2)
-        return (1);
-    return (check_prime(n, divisor + 1));
+if (n <= 1)
+return (0);
+if (n % divisor == 0 && divisor != n)
+return (0);
+if (divisor > n / 2)
+return (1);
+return (check_prime(n, divisor + 1));
 }
