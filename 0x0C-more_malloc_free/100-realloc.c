3c1640be2169d80b42be798c615c9d9bd740fde1
Cassey-M <murongacassey@gmail.com>
2023-06-05 08:17:21 -0700

    addv

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,33 +1,26 @@
 #include <stdlib.h>
+#include "main.h"
 /**
-* _realloc - Reallocates a memory block using malloc and free.
-* @ptr: Pointer to the memory previously allocated with malloc.
-* @old_size: Size of the allocated space for ptr.
-* @new_size: New size of the memory block.
+* _realloc - Reallocates a memory block using malloc and free
+* @ptr: Pointer to the memory previously allocated with malloc(old_size)
+* @old_size: Size, in bytes, of the allocated space for ptr
+* @new_size: New size, in bytes, of the new memory block
 *
-* Return: Pointer to the reallocated memory block.
-*         If new_size is equal to zero and ptr is not NULL, it returns NULL.
-*         If ptr is NULL, it is equivalent to malloc(new_size).
-*         If malloc fails, it returns NULL.
+* Return: Pointer to the reallocated memory block
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+char *new_ptr, *old_ptr;
 unsigned int i;
-char *src = ptr;
-char *dest = new_ptr;
-void *new_ptr;
-src = ptr;
-dest = new_ptr;
 if (new_size == old_size)
 return (ptr);
 if (ptr == NULL)
 {
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
-exit(98);
-return (new_ptr);
-}
-if (new_size == 0)
+return (NULL);
+return (new_ptr);	}
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
@@ -35,10 +28,9 @@ return (NULL);
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 return (NULL);
-if (new_size < old_size)
-old_size = new_size;
-for (i = 0; i < old_size; i++)
-dest[i] = src[i];
+old_ptr = ptr;
+for (i = 0; i < old_size && i < new_size; i++)
+new_ptr[i] = old_ptr[i];
 free(ptr);
 return (new_ptr);
 }
--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,89 +1,111 @@
 #include <stdlib.h>
+#include "main.h"
 /**
- * _strlen - Calculates the length of a string.
- * @s: The string.
- *
- * Return: The length of the string.
- */
+* _isdigit - Checks if a character is a digit (0-9)
+* @c: The character to be checked
+*
+* Return: 1 if the character is a digit, 0 otherwise
+*/
+int _isdigit(int c)
+{
+return (c >= '0' && c <= '9');
+}
+/**
+* _strlen - Calculates the length of a string
+* @s: The string to be measured
+*
+* Return: The length of the string
+*/
 unsigned int _strlen(char *s)
 {
-    unsigned int len = 0;
-
-    while (*s)
-    {
-        len++;
-        s++;
-    }
-
-    return len;
+unsigned int len = 0;
+while (*s++)
+len++;
+return (len);
 }
-
 /**
- * _isdigit - Checks if a character is a digit.
- * @c: The character to check.
- *
- * Return: 1 if the character is a digit, 0 otherwise.
- */
-int _isdigit(char c)
+* _puts - Prints a string to the standard output
+* @str: The string to be printed
+*/
+void _puts(char *str)
 {
-    return (c >= '0' && c <= '9');
+while (*str)
+_putchar(*str++);
 }
-int _putchar(char c);
-
 /**
- * mul - Multiplies two positive numbers.
- * @num1: The first number.
- * @num2: The second number.
- */
+* print_error_and_exit - Prints "Error" to the standard output and exits
+*/
+void print_error_and_exit(void)
+{
+_puts("Error");
+_putchar('\n');
+exit(98);
+}
+/**
+* mul - Multiplies two positive numbers
+* @num1: The first number as a string
+* @num2: The second number as a string
+*/
 void mul(char *num1, char *num2)
 {
-    unsigned int len1, len2, len, i, j;
-    int *result;
-
-    len1 = _strlen(num1);
-    len2 = _strlen(num2);
-    len = len1 + len2;
-
-    result = malloc(sizeof(int) * len);
-    if (result == NULL)
-        exit(98);
-
-    for (i = 0; i < len; i++)
-        result[i] = 0;
-
-    for (i = len1 - 1; i < len1; i--)
-    {
-        if (!_isdigit(num1[i]))
-        {
-            free(result);
-            exit(98);
-        }
-
-        for (j = len2 - 1; j < len2; j--)
-        {
-            if (!_isdigit(num2[j]))
-            {
-                free(result);
-                exit(98);
-            }
-
-            result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-            result[i + j] += result[i + j + 1] / 10;
-            result[i + j + 1] %= 10;
-        }
-    }
-
-    i = 0;
-    while (i < len - 1 && result[i] == 0)
-        i++;
-
-    while (i < len)
-    {
-        _putchar(result[i] + '0');
-        i++;
-    }
-
-    _putchar('\n');
-
-    free(result);
+int len1, len2, i, j, n1, n2, carry, digit;
+int *result;
+len1 = _strlen(num1);
+len2 = _strlen(num2);
+result = malloc((len1 + len2) * sizeof(int));
+if (result == NULL)
+print_error_and_exit();
+for (i = 0; i < len1 + len2; i++)
+result[i] = 0;
+for (i = len1 - 1; i >= 0; i--)
+{
+n1 = num1[i] - '0';
+carry = 0;
+for (j = len2 - 1; j >= 0; j--)
+{
+n2 = num2[j] - '0';
+digit = n1 * n2 + result[i + j + 1] + carry;
+carry = digit / 10;
+result[i + j + 1] = digit % 10;
+}
+if (carry > 0)
+result[i + j + 1] += carry;
+}
+i = 0;
+while (result[i] == 0)
+i++;
+if (i == len1 + len2)
+_putchar('0');
+else
+{
+while (i < len1 + len2)
+_putchar(result[i++] + '0');
+}
+_putchar('\n');
+free(result);
+}
+/**
+* main - Entry point
+* @argc: The number of command-line arguments
+* @argv: An array containing the command-line arguments
+*
+* Return: 0 on success, 98 on failure
+*/
+int main(int argc, char *argv[])
+{
+int i, j;
+if (argc != 3)
+{
+print_error_and_exit();
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (!_isdigit(argv[i][j]))
+print_error_and_exit();
+}
+}
+mul(argv[1], argv[2]);
+return (0);
 }
